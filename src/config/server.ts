24a54a27a0399a67ee68a9d07225e1594f30c99e
Oleg Marchenko <rosstofor@gmail.com>
2021-10-23 01:34:19 +0300

    create basic project structure

---
+++ src/config/server.ts
@@ -0,0 +1,4 @@
+import Dotenv from "dotenv";
+Dotenv.config();
+
+export const server_port = process.env.SERVER_PORT;
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import { server_port } from "./config/server";
+import db from "./db/models";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const start = async () => {
+  try {
+    db.sequelize.authenticate().then(() => {
+      app.listen(server_port, () => {
+        console.log(`Started on ${server_port}`);
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
